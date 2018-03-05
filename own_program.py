d8424add72a6cdfc40c0baf6d36ebb97ac1ac200
Philip Mirbach <philip.mirbach@gmail.com>
2018-03-05 17:46:34 +0100

    H, minimizing with a**2+b**2=1, display of eigs.

--- own_program.py
+++ own_program.py
@@ -8,8 +8,14 @@ Created on Fri Mar  2 14:27:59 2018
 
 import numpy as np
 from numpy import linalg as LA
+
 from matplotlib import pyplot as plt
 from numpy import sqrt
+
+from scipy.linalg import block_diag
+from scipy.optimize import minimize
+
+from itertools import product
 
 
 U_vec = np.linspace(0,10,50)
@@ -71,35 +77,178 @@ def E44(U,t):
 
 
 
-Phi_0 = np.array([1,0,0,1,0,0])
+Phi_0 = np.zeros(12)
+Phi_1 = np.zeros(12)
+Phi_2 = np.zeros(12)
+Phi_3 = np.zeros(12)
+Phi_4 = np.zeros(12)
 
+# Phi_0 = |0011>
+Phi_0[6] = Phi_0[9] = 1
 
-Phi_1 = np.array([1,0,0,1,0,0])
+# Phi_1 = |0022>
+Phi_1[2] = 1
 
-Phi_2 = np.array([0,0,1,0,0,1]) 
+# Phi_2 = |0033>
+Phi_2[6] = -1
+Phi_2[9] = 1
+
+#Phi_3 = |1122>
+Phi_3[-1] = Phi_3[-4] = 1
+
+#Phi_4 = |2233>
+Phi_4[-1] = 1
+Phi_4[-4] = -1
+
 
 
 E = np.zeros(U_vec.size)
+E_HF = np.zeros(U_vec.size)
 E_0 = np.zeros(U_vec.size)
 E_1 = np.zeros(U_vec.size)
 E_2 = np.zeros(U_vec.size)
+E_3 = np.zeros(U_vec.size)
+E_4 = np.zeros(U_vec.size)
+
+
+
+def E_Psi_0(x):
+    # a |0011> + b|0022>
+    Psi = x[0] * Phi_0 + x[1] * Phi_1
+    return np.min(np.dot(Psi, H * Psi ))
+
+def E_Psi_1(x):
+    # a |0011> + b|0033>
+    Psi = x[0] * Phi_0 + x[1] * Phi_2
+    return np.min(np.dot(Psi, H * Psi ))
+
+def E_Psi_2(x):
+    # a |0011> + b|1122>
+    Psi = x[0] * Phi_0 + x[1] * Phi_3
+    return np.min(np.dot(Psi, H * Psi ))
+
+def E_Psi_3(x):
+    # a |0011> + b|2233>
+    Psi = x[0] * Phi_0 + x[1] * Phi_4
+    return np.min(np.dot(Psi, H * Psi ))
+
+def E_Psi_4(x):
+    # a |0011> + b|0022> + c|0033>
+    Psi = x[0] * Phi_0 + x[1] * Phi_1 + x[2] * Phi_2
+    return np.min(np.dot(Psi, H * Psi ))
+
+x_0 = np.ones(2)
+cons = {'type': 'eq', 'fun': lambda x: np.dot(x,x) - 1}
+
+x_fun_0 = []
+x_fun_1 = []
+x_fun_2 = []
+x_fun_3 = []
+x_fun_4 = []
+
+eigs = np.zeros((12,U_vec.size))
+
 
 for i in np.arange(U_vec.size):
+
+    H1 = A44(U_vec[i],t=1)
+    H2 = E44(U_vec[i],t=1)
+    H = block_diag(H1,H2)
     
-    H = E44(U_vec[i],1)
     w,v = LA.eig(H)
+    
+    idx = w.argsort()
+    
+    E[i] = w[idx[0]]
+    eigs[:,i] = v[:,idx[0]]
+    
     
 #    if i == 0:
 #        E = np.zeros((U_vec.size,w.size))
     
 #    E[i,:] = np.sort(w)
-    E[i] = np.min(w)
-    E_0[i] = np.min(np.dot(Phi_0, H * Phi_0 ))
-    E_2[i] = np.min(np.dot(Phi_2, H * Phi_2 ))
+    res_0 = minimize(E_Psi_0, x_0, method='SLSQP',constraints=cons)
+    res_1 = minimize(E_Psi_1, x_0, method='SLSQP',constraints=cons)
+    res_2 = minimize(E_Psi_2, x_0, method='SLSQP',constraints=cons)
+    res_3 = minimize(E_Psi_3, x_0, method='SLSQP',constraints=cons)
+#    res_4 = minimize(E_Psi_4, (1,1,1), method='SLSQP',constraints=cons)
+    
+#    print(res)
+    
+    E_0[i] = res_0.fun
+    E_1[i] = res_1.fun
+    E_2[i] = res_2.fun
+    E_3[i] = res_3.fun
+#    E_4[i] = res_4.fun
+    
+    x_fun_0.append((res_0.x, res_0.fun))
+    x_fun_1.append((res_1.x, res_1.fun))
+    x_fun_2.append((res_2.x, res_2.fun))
+    x_fun_3.append((res_3.x, res_3.fun))
+#    x_fun_4.append((res_4.x, res_4.fun))
+    
+    
+    E_HF[i] = np.min(np.dot(Phi_0, H * Phi_0 ))
+#    E_1[i] = np.min(np.dot(Phi_1, H * Phi_1 ))
+#    E_2[i] = np.min(np.dot(Phi_2, H * Phi_2 ))
+#    E_3[i] = np.min(np.dot(Phi_3, H * Phi_3 ))
+#    E_4[i] = np.min(np.dot(Phi_4, H * Phi_4 ))
 
 
 
-#print(np.dot(HF_ansatz, H * HF_ansatz))
+
+
+fig, ax = plt.subplots()
+
+ax.plot(U_vec,E,label='exact',color='blue')
+ax.plot(U_vec,E_HF,'--',label='|0011> HF',color='blue')
+
+ax.plot(U_vec,E_0,ls=':',color='black',linewidth=3,label='a|0011> + b |0022>')
+ax.plot(U_vec,E_1,ls=':',color='red',linewidth=1,label='a|0011> + b |0033>')
+ax.plot(U_vec,E_2,ls=':',color='green',linewidth=1,label='a|0011> + b |1122>')
+ax.plot(U_vec,E_3,ls=':',color='grey',linewidth=1,label='a|0011> + b |2233>')
+
+#ax.plot(U_vec,E_4,ls='-.',color='red',linewidth=1,label='a|0011> + b |0022> + c |0033>')
+
+#ax.plot(U_vec,E_1,ls='-',color='black',linewidth=3,label='|0022>')
+#ax.plot(U_vec,E_2,ls='-.',label='|0033>')
+#ax.plot(U_vec,E_3,ls='-.',label='|1122>')
+#ax.plot(U_vec,E_4,ls=':',label='|1133>')
+
+ax.set(ylabel='energy E', xlabel='U/t')
+ax.legend(loc='best')
+fig.show()
+
+
+
+
+
+#fig2, ax2 = plt.subplots()
+##a_list = [el_x_fun_0[0][0] for el_x_fun_0 in x_fun_0]
+##b_list = [el_x_fun_0[0][1] for el_x_fun_0 in x_fun_0]
+#
+#a_list = [el[0][0] for el in x_fun_3]
+#b_list = [el[0][1] for el in x_fun_3]
+#
+#ax2.plot(a_list)
+#ax2.plot(b_list,'--')
+#fig2.show()
+
+
+fig3, ax3 = plt.subplots()
+
+markers = ["-", "--", "x"]
+colors = ["b", "g", "r", "c", "m", "y", "k"]
+ls = [a + b for a, b in product(markers, colors)]
+
+
+for i in range(eigs.shape[0]):
+    ax3.plot(U_vec, eigs[i,:], ls[i], label=i)
+
+
+ax3.legend(loc='best')
+
+fig3.show()
 
 
 
@@ -108,13 +257,6 @@ for i in np.arange(U_vec.size):
 
 
 
-
-
-
-plt.plot(U_vec,E)
-plt.plot(U_vec,E_0)
-plt.plot(U_vec,E_2)
-plt.show()
 
 
 
