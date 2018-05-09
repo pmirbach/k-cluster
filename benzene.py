0773f11483b7daac1878fd6ab96965151775d829
Philip Mirbach <philip.mirbach@gmail.com>
2018-05-09 17:27:59 +0200

    Extraction of sparse operators.

--- benzene.py
+++ benzene.py
@@ -37,7 +37,7 @@ spStr = ['_up', '_dn']
 
 #uVec = np.arange(0.0,20.0,0.2)
 uVec = np.linspace(start=0.0, stop=8.0, num=50)
-#uVec = np.array([2.0])
+#uVec = np.array([7.6])
 #betaVec = np.array([20.0])
 
 par.beta = 20.0
@@ -45,19 +45,75 @@ par.beta = 20.0
 Energy = np.zeros(uVec.size)
 
 
-print()
-print('-------------------------')
-print()
+print('\n{}\n'.format(('-')*70))
 
 
 v = np.max(ham['diagblk_dim'])
 ind = np.argmax(ham['diagblk_dim'])
 
-eig_vecs_save = np.zeros((v, v, uVec.size))
+eig_vecs_save = np.zeros((v, uVec.size))
+eig_vals_save = np.zeros((v, uVec.size))
+
+
+#for x,y in ham['oper'].items():
+#    print(x, type(y))
+#    
+#    print(y.nnz)
+#    
+##    rows, cols = y.nonzero()
+##    print(rows, cols)
+##    print(y.shape)
+#    print(dir(y))
+#    print(y.format)
+#    print(y.data)
+#    
+#    z = y.tocoo()
+#    z2 = z.toarray()
+#    
+#    print(z2)
+#    
+#    plt.imshow(z2)
+#    plt.show()
+#    
+#    
+#    break
+
+#a = np.arange(256)
+#print(a[110:146])
 
 
 
-#exit()
+
+ind0, ind1 = ham['diagblk_ind'][ind], ham['diagblk_ind'][ind+1]
+
+
+asdf = ham['oper']['+imp1_up'] * ham['oper']['-imp1_up']
+asdf2 = asdf.tocoo()
+asdf3 = asdf2.toarray()
+
+#ham['oper']['+imp'+str(ii+1)+'_up']
+
+plt.imshow(asdf3[ind0:ind1,ind0:ind1])
+plt.show()
+
+#fig, axes = plt.subplots(nrows=2, ncols=2)
+#
+#fig_manager = plt.get_current_fig_manager()
+#if hasattr(fig_manager, 'window'):
+#    fig_manager.window.showMaximized()
+#
+##print(axes)
+#for i, ax in enumerate(fig.axes):
+#    ax.set(title=i)
+##    axes[i].imshow()
+#
+#plt.show()
+#
+#print('\n{}\n'.format(('-')*70))
+    
+exit()
+
+
 
 
 t = 1.0 if par.NimpOrbs > 2.0 else 0.5
@@ -82,9 +138,14 @@ for iU in range(uVec.size):
     
         
     eigVals,eigVecs = hel.diagonalize_blocks(H,ham['diagblk_qnum'],ham['diagblk_dim'],ham['diagblk_ind'],'full',cfg['ed'])
-    eig_vecs_save[:,:,iU] = np.array(eigVecs[ind])
+    
+    
+    eig_vals_save[:,iU] = np.array(eigVals[ind])
+    eig_vecs_save[:,iU] = eigVecs[ind][:,0]
+    
     
     ZZ1Til,ZZ1BlockTil,Phi,NG,E0,eigVecs,thermo,minIndex,E0Index = hel.partitionFunction(par,ham,eigVals,eigVecs,cfg)
+    
     
 #    for jj in range(par.NimpOrbs//2+1):
 #        a=hel.calcExpecValueRes(eigVecs,ham['diagblk_ind'],ham['diagblk_qnum'],NG,ZZ1BlockTil,ZZ1Til,
@@ -98,34 +159,48 @@ for iU in range(uVec.size):
     Energy[iU] = hel.calcExpecValueRes(eigVecs,ham['diagblk_ind'],ham['diagblk_qnum'],NG,ZZ1BlockTil,ZZ1Til,
                                         H,cfg,par,thermo)
 
-#x = ham['oper']['+imp1_up']
-#
-#print(type(x))
-#x2 = x.todense()
 
-#eig_test = eig_vecs_save[:,0,0]
-#print(eig_test, np.dot(eig_test, eig_test))
-#
-#eig_test = eig_vecs_save[0,:,0]
-#print(eig_test, np.dot(eig_test, eig_test))
-
-
-print(np.transpose(eig_vecs_save[0,:,-1]))
-exit()
-
-eig_plot = np.transpose(eig_vecs_save[0,:,-1])
 
 fig, ax = plt.subplots()
 
-ax.plot(eig_plot)
+#ax.plot(eig_plot)
 #for i in range(v):
 #    y = np.transpose((eig_plot[i,:]))
 #    ax.plot(uVec, np.sort(y),'--')
 
 
 #plt.plot(uVec,Energy)
-plt.show()
-    
+
+
+#for i in range(v):
+#    ax.plot(uVec, eig_vals_save[i,:])
+#
+#plt.show()
+#    
+for x in eig_vecs_save[:,-1]:
+    print(x)
+
+
+#for x in ham['oper']:
+#    print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
